f637d6ae8acaa8eac2c223e72e90a69e37d49681
Brady Alleman <brady@alleman.me>
2018-01-07 16:56:52 -0500

    initially working

--- mantis_recurring_task_monitor.py
+++ mantis_recurring_task_monitor.py
@@ -6,6 +6,7 @@ import requests
 import datetime
 import dateutil.parser
 import pytz
+import copy
 
 parser = argparse.ArgumentParser(description='Manage recurring tasks in Mantis')
 parser.add_argument('--api', help='URL to Mantis REST API', required=True)
@@ -20,9 +21,10 @@ post_headers = { 'Authorization': args.token, 'Content-Type': 'application/json'
 r_proj = requests.get(args.api + '/projects', headers=headers)
 
 project_id = -1
+projects = {} 
 for project in r_proj.json()['projects']:
-    if project['name'] == args.project:
-        project_id=project['id']
+    projects[project['name']] = project['id']
+project_id = projects[args.project]
 
 if project_id == -1:
     print json.dumps(r_proj.json(), indent=2)
@@ -52,18 +54,20 @@ for seed in r_seed.json()['issues']:
     last_update = -1
     last_update_open = dateutil.parser.parse("1970-01-01T00:00:00-05:00") 
     last_open = -1
-    for rel in seed['relationships']:
-        if rel['type']['name'] == "has instance":
-            r_rel = requests.get(args.api + '/issues/' + str(rel['target_id']), headers=headers)
-            issue = r_rel.json()["issues"][0]
-            updated = dateutil.parser.parse(issue["updated_at"])
-            if updated > last_update_all:
-                last_update_all = updated
-                last_update = issue['id']
-            if issue['status']['name'] != 'closed' and updated > last_update_open:
-                last_update_open = updated
-                last_open = issue['id']
-            print json.dumps(r_rel.json(), indent=2)
+    if 'relationships' in seed:
+        for rel in seed['relationships']:
+            if rel['type']['name'] == "has instance":
+                r_rel_issue = requests.get(args.api + '/issues/' + str(rel['target_id']), headers=headers)
+                #print r_rel_issue.text
+                issue = r_rel_issue.json()["issues"][0]
+                updated = dateutil.parser.parse(issue["updated_at"])
+                if updated > last_update_all:
+                    last_update_all = updated
+                    last_update = issue['id']
+                if issue['status']['name'] != 'closed' and updated > last_update_open:
+                    last_update_open = updated
+                    last_open = issue['id']
+                #print json.dumps(r_rel_issue.json(), indent=2)
     print "  Last Updated Instance was " + str(last_update) + " at " + str(last_update_all)
     print "  Last Open Updated Instance is " + str(last_open) + " at " + str(last_update_open)
     if remind_days > 0 and last_open != -1 and datetime.datetime.now(pytz.utc) > last_update_open + datetime.timedelta(days=remind_days):
@@ -71,9 +75,24 @@ for seed in r_seed.json()['issues']:
         print "We need to update " + str(last_open) + " with a reminder note"
         note = {'text': "This is a reminder to complete this recurring task." }
         r_note = requests.post(args.api + '/issues/' + str(last_open) + '/notes', headers=post_headers, data=json.dumps(note))
-        print r_note.text
-    if recur_days > 0 and last_open == -1 and last_update != -1 and datetime.datetime.now(pytz.utc) > last_update_all + datetime.timedelta(days=recur_days):
+        print "Created note " + str(r_note.json()['note']['id'])
+    if recur_days > 0 and last_open == -1 and (last_update != -1 and datetime.datetime.now(pytz.utc) > last_update_all + datetime.timedelta(days=recur_days)) or last_update == -1:
         # if we have a closed instance, and the recur interval has expired based on its last update
         print "We need to create a new instance for seed " + str(seed['id'])
+        n_issue = copy.deepcopy(seed)
+        del n_issue['id']
+        n_issue['notes'] = []
+        n_issue['relationships'] = []
+        n_issue['custom_fields'] = []
+        n_issue['project']['id'] = projects[instance_project]
+        n_issue['project']['name'] = instance_project
 
-    
+        r_issue = requests.post(args.api + '/issues', headers=post_headers, data=json.dumps(n_issue))
+        #print r_issue.text
+        j_issue = r_issue.json()
+        print "Created issue " + str(j_issue['issue']['id'])
+
+        relationship = { 'issue': { 'id': seed['id'] }, 'type': { 'id': 99 } }
+        r_rel = requests.post(args.api + '/issues/' + str(j_issue['issue']['id']) + '/relationships', headers=post_headers, data=json.dumps(relationship))
+        #print r_rel.text
+
